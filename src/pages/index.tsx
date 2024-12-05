9e9ddaa99aab156fa17ad813128a0ea64988a61f
Fernando Garrido <siberiancoffe@outlook.cl>
2024-12-05 18:21:47 -0300

    feat: Add social media links and enhance contact section in homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,12 @@ import Heading from "@theme/Heading";
 
 // icons
 import { IoMail } from "react-icons/io5";
-import { FaRegNewspaper } from "react-icons/fa6";
+import {
+	FaLinkedin,
+	FaGithub,
+	FaCircleInfo,
+	FaRegNewspaper,
+} from "react-icons/fa6";
 
 const HomePageHeader = (): JSX.Element => {
 	return (
@@ -30,34 +35,68 @@ const HomePageHeader = (): JSX.Element => {
 					<p className="text-2xl">
 						<b>Fernando Garrido</b>
 					</p>
+					<div className="flex justify-center gap-3">
+						<Link
+							className="transition delay-0 duration-200 hover:opacity-55"
+							style={{
+								color: "inherit",
+							}}
+							to="https://www.linkedin.com/in/fernandogarridodev/"
+						>
+							<FaLinkedin size={45} />
+						</Link>
+						<Link
+							className="transition delay-0 duration-200 hover:opacity-55"
+							style={{
+								color: "inherit",
+							}}
+							to="https://github.com/CoffeSiberian/"
+						>
+							<FaGithub size={45} />
+						</Link>
+					</div>
+					<div>
+						<Link
+							className="flex items-center justify-center gap-3 transition delay-0 duration-200 hover:opacity-55"
+							style={{
+								color: "inherit",
+							}}
+							to="mailto:siberiancoffe@outlook.cl"
+						>
+							<IoMail size={45} />
+							<div className="text-lg font-semibold">
+								siberiancoffe@outlook.cl
+							</div>
+						</Link>
+					</div>
 				</div>
 				<div className="flex flex-col items-center gap-3">
 					<Heading as="h1" className="hero__title">
 						Desarrollador Full Stack
 					</Heading>
-					<p className="hero__subtitle max-w-3xl">
+					<p className="hero__subtitle max-w-3xl font-semibold">
 						Soy un apasionado desarrollador full stack con experiencia en
-						tecnologías como JavaScript, TypeScript, Python y Rust. He llevado a
-						cabo diversos proyectos personales, desde la creación de interfaces
-						de usuario interactivas hasta la implementación de servicios backend
-						escalables, donde suelo destacar mi habilidad de abstracción y
-						entendimiento técnico de bajo nivel, que es donde más especialidad
-						tengo, entregando resultados de calidad.
+						tecnologías como <b>JavaScript, TypeScript, Python y Rust</b>. He
+						llevado a cabo diversos proyectos personales, desde la creación de
+						interfaces de usuario interactivas hasta la implementación de
+						servicios backend escalables, donde suelo destacar mi habilidad de
+						abstracción y entendimiento técnico de bajo nivel, que es donde más
+						especialidad tengo, entregando resultados de <b>calidad</b>.
 					</p>
 					<div className={"flex flex-col-reverse gap-3 md:flex-row"}>
-						<Link
-							className="button button--lg button--info flex items-center justify-center gap-1"
-							to="mailto:siberiancoffe@outlook.cl"
-						>
-							Contacto
-							<IoMail />
-						</Link>
 						<Link
 							className="button button--lg button--success flex items-center justify-center gap-1"
 							to="https://cdn.siberiancoffe.dev/cv/fernando_garrido_cv.pdf"
 						>
-							Descargar CV
+							Ver CV
 							<FaRegNewspaper />
+						</Link>
+						<Link
+							className="button button--lg button--warning flex items-center justify-center gap-1"
+							to="/blog/about"
+						>
+							Sobre Mi
+							<FaCircleInfo />
 						</Link>
 					</div>
 				</div>
